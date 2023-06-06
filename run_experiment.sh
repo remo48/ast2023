82cd982844988c63766f04de0c3d2f078be8f50d
Remo Kellenberger <remok@student.ethz.ch>
2023-06-06 20:15:54 +0200

    changed plot

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-opt_levels=("O3", "Os")
+opt_levels=("O3" "Os")
 compilers=("gcc" "clang")
 timeouts=(300 400)
-threshold=(100 200)
+thresholds=(100 200)
 
 for compiler in "${compilers[@]}"; do
   for opt_level in "${opt_levels[@]}"; do
     for timeout in "${timeouts[@]}"; do
       for threshold in "${thresholds[@]}"; do
-        python main.py --rounds 10 --opt-level "$opt_level" --compiler "$compiler" --timeout "$timeout" --threshold "$threshold" --out out_new
+        python main.py --rounds 10 --opt-level "$opt_level" --compiler "$compiler" --timeout "$timeout" --threshold "$threshold" --out out_2
       done
     done
   done
